4927c2c2309f44c60ce9c1fd60258ee4118e9c65
developer-shourav <mrinmoyraj823@gmail.com>
2025-04-07 22:42:34 +0600

    Login form added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,57 +1,52 @@
-import { Button } from "antd";
-import { FieldValues, useForm } from "react-hook-form";
-import { useLoginMutation } from "../redux/features/auth/authApi";
-import { useAppDispatch } from "../redux/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
-import { verifyToken } from "../utils/verifyToken";
-import { useNavigate } from "react-router";
-import { toast } from "sonner";
-import { TUser } from "../types";
+import { Button, Row } from 'antd';
+import { FieldValues } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
+import { useAppDispatch } from '../redux/hooks';
+import { setUser } from '../redux/features/auth/authSlice';
+import { verifyToken } from '../utils/verifyToken';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+import UMForm from '../components/form/UMForm';
+import UMInput from '../components/form/UMInput';
+import { TUser } from '../types';
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      userId: "A-0001",
-      password: "HareKrishna143Hare",
-    },
-  });
-  const [loginUser] = useLoginMutation();
+  const defaultValues = {
+    userId: '2027030006',
+    password: 'hareKrishna',
+  };
+
+  const [login] = useLoginMutation();
 
   const onSubmit = async (data: FieldValues) => {
-    const toastId = toast.loading("login in..", { duration: 2000 });
+    const toastId = toast.loading('Logging in', {duration: 2000});
+
     try {
       const userInfo = {
         id: data.userId,
         password: data.password,
       };
-
-      const res = await loginUser(userInfo).unwrap();
+      const res = await login(userInfo).unwrap();
       const user = verifyToken(res.data.accessToken) as TUser;
-
       dispatch(setUser({ user: user, token: res.data.accessToken }));
-      toast.success("Logged In", { id: toastId, duration: 2000 });
-
-      navigate(`/${user?.role}/dashboard`);
+      toast.success('Logged in', { id: toastId, duration: 2000 });
+      navigate(`/${user.role}/dashboard`);
     } catch (err) {
-      toast.error("Something went wrong", { id: toastId, duration: 2000 });
+      toast.error('Something went wrong', { id: toastId, duration: 2000 });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="id">ID: </label>
-        <input type="text" id="id" {...register("userId")} />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </form>
+    <Row justify="center" align="middle" style={{ height: '100vh' }}>
+      <UMForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <UMInput type="text" name="userId" label="ID:" />
+        <UMInput type="text" name="password" label="Password" />
+        <Button htmlType="submit">Login</Button>
+      </UMForm>
+    </Row>
   );
 };
 
