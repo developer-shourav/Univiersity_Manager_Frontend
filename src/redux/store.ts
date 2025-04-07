8c85ecd5730c6b063580bc37e31bc5d4ed72b58f
developer-shourav <mrinmoyraj823@gmail.com>
2025-04-07 17:28:28 +0600

    Non Serialaized object error solved

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,22 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice';
 import { baseApi } from "./api/baseApi";
-import {persistReducer, persistStore} from "redux-persist";
+import {
+    persistReducer, persistStore, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
-key: 'auth',
-storage,
+    key: 'auth',
+    storage,
 };
 
 const persistAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
     reducer: {
-        [baseApi.reducerPath] : baseApi.reducer,
+        [baseApi.reducerPath]: baseApi.reducer,
         auth: persistAuthReducer,
     },
-    middleware: getDefaultMiddlewares => getDefaultMiddlewares().concat(baseApi.middleware)
+    middleware: getDefaultMiddlewares => getDefaultMiddlewares({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    }).concat(baseApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
